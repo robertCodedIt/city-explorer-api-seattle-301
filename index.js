fef0d34671fb9193b8e81ae14b8af49e050d9684
GOLD <robertlove99999@gmail.com>
2022-03-22 15:39:01 -0500

    got set up

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+const PORT = 3001;
+const user = { Name: "Robert", Year: new Date().getUTCFullYear() };
+const getHomePage = () => {
+  app.get("/", (req, res) => {
+    res.send(user);
+  });
+};
+
+app.listen(PORT, () => {
+  console.log(`I am listening on port ${PORT}`);
+  getHomePage()
+});
